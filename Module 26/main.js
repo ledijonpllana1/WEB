e27691b193cafd6a0c262ed6230991152df23a84
ledijonpllana1 <pllanaledijon7@gmail.com>
2026-03-24 19:19:19 +0100

    Module 26

---
+++ Module 26/main.js
@@ -0,0 +1,63 @@
+
+
+
+
+var x=5*4787
+
+
+console.log(x)
+
+
+var a=10
+var b=50
+var c=a+b
+console.log(c)
+
+var g=10
+var i=50
+var k=g-i
+console.log(k)
+
+var y=10
+var o=50
+var l=y%o
+console.log(l)
+
+
+var q=10
+var w=50
+var v=q/w
+console.log(l)
+
+
+var m=10
+m++
+console.log(m)
+
+
+
+var u=10
+u--
+console.log(u)
+
+var p=10
+var q=p**2
+console.log(q)
+
+
+var y=20;
+y /=4
+console.log(y)
+
+
+var b1=11
+var b6=30
+
+console.log(b1>b6)
+console.log(b1<b6)
+console.log(b1>=b6)
+
+var name="filan";
+var surname="fisteku"
+
+console.log(name+""+surname)
