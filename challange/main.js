7b6a5e1eda325a0b9f59f3348e431f780a21b02b
ledijonpllana1 <pllanaledijon7@gmail.com>
2026-04-30 19:01:35 +0200

    Module 33

---
+++ challange/main.js
@@ -0,0 +1,8 @@
+var circle = document.getElementById('circle');
+
+function showCircle(){
+    circle.style.display = 'block';
+    circle.style.top = Math.random() * 500 + 'px';
+    circle.style.top = Math.random() * 900 + 'px';
+}
+
