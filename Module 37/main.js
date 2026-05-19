3a726dc54ce0491948a5cd0de6d5c4dda7be35cf
ledijonpllana1 <pllanaledijon7@gmail.com>
2026-05-19 19:12:21 +0200

    Module 37

---
+++ Module 37/main.js
@@ -0,0 +1,19 @@
+$('#sort').sortable();
+
+$(function(){
+    $('#accordion').accordion();
+});
+
+$(function(){
+    $('#accordion').accordion({
+        collapsible: true
+    });
+});
+
+$('#tabs').tabs();
+
+$(function(){
+      $('#tabs').tabs({
+        collapsible: true
+    });
+})
